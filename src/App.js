f80ce726b0cdd29ab3e30d45f0f352bca22e85ba
TomasSchlogl <thomas537@seznam.cz>
2021-08-19 16:58:13 +0200

    active link done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/header";
 import * as ROUTES from "./constants/routes";
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.scss";
 
 const Home = lazy(() => import("./pages/home"));
 const Showcase = lazy(() => import("./pages/showcase"));
@@ -16,7 +17,7 @@ export default function App() {
           <Header />
           <Switch>
             <Route path={ROUTES.HOME} component={Home} exact />
-            <Route path={ROUTES.SHOWCASE} component={Showcase} />
+            <Route path={ROUTES.SHOWCASE} component={Showcase} exact />
             <Route path={ROUTES.CONTACTS} component={Contacts} />
             <Route path={ROUTES.OFFER} component={Offer} />
           </Switch>
--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,43 @@
 import * as ROUTES from "../constants/routes";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <div className="header-container bg-dark-blue h-32 flex justify-end items-center">
-      <Link to={ROUTES.HOME}>
+      <NavLink to={ROUTES.HOME}>
         <h1 className="header-logo absolute left-3">DIANA</h1>
-      </Link>
+      </NavLink>
 
-      <ul className="flex">
-        <Link to={ROUTES.HOME}>
-          <li className="header-li">Úvod</li>
-        </Link>
-        <Link to={ROUTES.OFFER} exact>
-          <li className="header-li">Výběr</li>
-        </Link>
-        <Link to={ROUTES.SHOWCASE}>
-          <li className="header-li">Galerie</li>
-        </Link>
-        <Link to={ROUTES.CONTACTS}>
-          <li className="header-li">Kontakty</li>
-        </Link>
+      <ul className="flex text-white">
+        <NavLink
+          to={ROUTES.HOME}
+          exact
+          activeClassName="active-header"
+          className="header-li"
+        >
+          <li>Úvod</li>
+        </NavLink>
+        <NavLink
+          to={ROUTES.OFFER}
+          activeClassName="active-header"
+          className="header-li"
+        >
+          <li>Výběr</li>
+        </NavLink>
+        <NavLink
+          to={ROUTES.SHOWCASE}
+          activeClassName="active-header"
+          className="header-li"
+        >
+          <li>Galerie</li>
+        </NavLink>
+        <NavLink
+          to={ROUTES.CONTACTS}
+          activeClassName="active-header"
+          className="header-li"
+        >
+          <li>Kontakty</li>
+        </NavLink>
       </ul>
     </div>
   );
